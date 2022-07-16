48df6528e7308c758f3b6e999e94d71b4c4ce983
Aadarsh Panwar <aadarsh.panwar@kloudspot.com>
2022-07-17 00:05:08 +0530

    simple-web application completed

--- simple-web-application/index.js
+++ simple-web-application/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port =9898;
 app.get("/", (req, res) => {
-    res.send("Docker examples");
+    res.send("Docker example");
 });
 
 
